aca8ae3f6000334a72032835a32420aa92ba9d15
icantrell <icantrell617@gmail.com>
2018-07-17 08:51:09 -0700

    added class version that holds queue of size 100 and has frequency lookup method.

--- random_count_generator.py
+++ random_count_generator.py
@@ -1,7 +1,11 @@
 import random
-
+from queue import Queue
 
 def roulette(dist):
+    '''
+    given probability dist.
+    returns random number based on dist. 
+    '''
     thres = random.random()
     i = 0
     for p in dist.items():
@@ -9,14 +13,49 @@ def roulette(dist):
        if i >= thres:
            return p[0]
 
-def random_count_generator():
-    p = {1:0.5, 2:0.25, 3:0.15, 4:.05, 5:0.05}
-    return roulette(p)
+class RNG():
+    '''
+    Random number generator class
+    '''
+    def __init__(self, p):
+        '''
+        input: probablity dist.
+        '''
+        self.p = p
+        #queue is a synchronized data structure.
+        self.queue = Queue() 
+    
+    def generate(self):
+        '''
+        generates random number and remembers
+        last 100 numbers.
+        '''
+        freq = roulette(self.p)
+        #pop one off the front if len=100
+        if self.queue.qsize() == 100:
+            self.queue.get()
+
+        self.queue.put(freq)
+        return freq
+
+    def get_mapping(self, n):
+        '''
+        get the frequency of a number in the mapping
+        '''
+        if n not in self.p:
+            print(str(n) + ' not in probability dist.')
+            return None
+        return self.p[n]
 
 def rng_to_console():
+    '''
+    loop to output rng numbers in console
+    '''
+    rng = RNG(p = {1:0.5, 2:0.25, 3:0.15, 4:0.05, 5:0.05})
     while(1):
         input('press <ENTER> to see random number.')
-        print(random_count_generator())
+        print(rng.generate())
 
+#if run as standalone.
 if __name__=='__main__':
     rng_to_console()
--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ from collections import Counter
 from math import isclose
 
 def test_rng():
-    n = 10000000
-    a = [random_count_generator.random_count_generator() for i in range(n)]
+    rng = random_count_generator.RNG(p = {1:0.5, 2:0.25, 3:0.15, 4:0.05, 5:0.05})
+
+    n = 1000000
+    a = [rng.generate() for i in range(n)]
 
     c = Counter(a)
     assert(isclose(c[1]/n, 0.5, rel_tol=0.01)) 
@@ -12,3 +14,9 @@ def test_rng():
     assert(isclose(c[3]/n, 0.15, rel_tol=0.01)) 
     assert(isclose(c[4]/n, 0.05, rel_tol=0.01)) 
     assert(isclose(c[5]/n, 0.05, rel_tol=0.01)) 
+
+    assert(rng.queue.qsize() == 100)
+
+    assert(rng.get_mapping(2) == 0.25)
+    assert(rng.get_mapping(5) == 0.05)
+    assert(rng.get_mapping(7) == None)
